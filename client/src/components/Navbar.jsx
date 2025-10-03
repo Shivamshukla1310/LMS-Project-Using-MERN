1684a8c2a3f88a7c643c2ef519bb16ea819e9564
Shivam Shukla <shukla.shivamv@gmail.com>
2025-10-03 23:39:15 +0530

    Using Sheet for mobile device from shadcn

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,18 +4,20 @@ import React from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from './ui/dropdown-menu';
 import { Button } from './ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import DarkMode from '@/DarkMode';
 // Here We Are using Lucide React in this program
 const user = true;
 const Navbar = () => {
   return (
     <div className='h-16 dark:bg-[#0A0A0A] bg-white border b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10'>
+      {/* For Desktop */}
       <div className='max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full'>
         <div className='flex items-center gap-2'>
           <School size={"30"} />
           <h1 className='hidden md:block font-extrabold text-2xl'>Moodle</h1>
         </div>
         {/* User icons and darkmode icon  */}
-        <div>
+        <div className='flex items-center gap-10'>
           {
             user ? (
               <DropdownMenu>
@@ -43,15 +45,52 @@ const Navbar = () => {
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
-            ) : <div className='flex items-center gap-2'>
+            ) : (<div className='flex items-center gap-2'>
               <Button variant="outline">Login</Button>
               <Button>Sign Up</Button>
             </div>
-          }
+            )}
+          <DarkMode />
         </div>
       </div>
+      {/* For Mobile */}
+      <MobileNavbar />
     </div >
   )
 }
 
 export default Navbar
+
+const MobileNavbar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Input id="sheet-demo-username" defaultValue="@peduarte" />
+          </div>
+        </div>
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
