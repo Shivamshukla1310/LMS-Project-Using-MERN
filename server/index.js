b85f5806ba0447009ea437f9e1b62cf296cd7a83
Shivam Shukla <shukla.shivamv@gmail.com>
2025-10-05 00:56:24 +0530

    Routing and controllers with some lecture and course model are been updated

--- server/index.js
+++ server/index.js
@@ -3,39 +3,43 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./database/db.js";
-import userRoute from "./routes/user.routes.js";
+import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
+import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 
 dotenv.config({});
 
-// Call Database connection Here
+// call database connection here
 connectDB();
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// default middlewares
+// default middleware
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
-
-// Apis
-app.use("/api/v1/user", userRoute);
-
-app.get("/home", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: "Hello i am coming from backend"
-  })
-})
 
 // routes...
 // app.post("/api/v1/user/register", (req, res) => {
 //   res.json({ message: "User registered successfully" });
 // });
 
+// apis
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
+
+
 app.listen(PORT, () => {
-  console.log(`Server listen at Port ${PORT}`);
+  console.log(`Server listen at port ${PORT}`);
 })
+
