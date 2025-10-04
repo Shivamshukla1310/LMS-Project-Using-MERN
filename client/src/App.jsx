73a2b35ddee86ab55a7ac17446abccc70a6d9b3f
Shivam Shukla <shukla.shivamv@gmail.com>
2025-10-05 03:15:17 +0530

    Everything is working fine but the payment gateway is still in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,9 @@ import Dashboard from './pages/admin/Dashboard'
 import CourseTable from './pages/admin/course/CourseTable'
 import AddCourse from './pages/admin/course/AddCourse'
 import EditCourse from './pages/admin/course/EditCourse'
+import CreateLecture from './pages/admin/Lectures/CreateLecture'
+import EditLecture from './pages/admin/Lectures/EditLecture'
+import CourseDetail from './pages/student/CourseDetail'
 
 const appRouter = createBrowserRouter([
   {
@@ -42,6 +45,11 @@ const appRouter = createBrowserRouter([
         element: <Profile />
 
       },
+      {
+        path: "course-detail/:courseId",
+        element: <CourseDetail />
+
+      },
 
       // admin routes starts from here itself
 
@@ -64,6 +72,14 @@ const appRouter = createBrowserRouter([
           {
             path: "course/:courseId",
             element: <EditCourse />
+          },
+          {
+            path: "course/:courseId/lecture",
+            element: <CreateLecture />
+          },
+          {
+            path: "course/:courseId/lecture/:lectureId",
+            element: <EditLecture />
           }
         ]
       }
