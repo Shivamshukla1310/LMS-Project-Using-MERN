fd5f42f0e2b78f71955725a9bd77fafebcee9a9a
Shivam Shukla <shukla.shivamv@gmail.com>
2025-11-02 00:26:47 +0530

    Added Frontend URL into the .env of backend

--- server/index.js
+++ server/index.js
@@ -21,10 +21,26 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "http://localhost:5173",
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: (origin, cb) => {
+    // allow requests with no origin (like Postman) and check deployed origin
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 
 // routes...
 // app.post("/api/v1/user/register", (req, res) => {
