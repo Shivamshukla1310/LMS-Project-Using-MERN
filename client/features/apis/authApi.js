087705b65873d1cf2c533858d828d5056bb725d0
Shivam Shukla <shukla.shivamv@gmail.com>
2025-11-01 22:57:17 +0530

    Project Updated And Base URL For Render Updated In The Frontend

--- client/features/apis/authApi.js
+++ client/features/apis/authApi.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
 // Base API URL
-const USER_API = "http://localhost:8080/api/v1/user";
+// const USER_API = "http://localhost:8080/api/v1/user";
+const USER_API = `${import.meta.env.VITE_BACKEND_URL}/user`;
+
 
 export const authApi = createApi({
   reducerPath: "authApi",
